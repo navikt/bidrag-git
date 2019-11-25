966f7716917dd160c842244a250bd1c2b27cff82
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-11-25 13:57:05 +0100

    read files with callback functions

--- prepare-release-mvn-pkg/index.js
+++ prepare-release-mvn-pkg/index.js
@@ -9,8 +9,28 @@ async function run() {
     // Execute verify bash script
     await exec.exec(`${__dirname}/src/prepare-release.sh`);
 
-    core.setOutput("tagged-release", fs.readFile(`${__dirname}/.tagged_release`));
-    core.setOutput("commit-sha", fs.readFile(`${__dirname}/.commit_sha`));
+    const encoding = {encoding: 'utf-8'};
+    var filepath = `${__dirname}/.tagged_release`;
+
+    fs.readFile(filepath, encoding, function(err,data){
+      if (!err) {
+        core.debug('read tagged release: ' + data);
+        core.setOutput("tagged-release", data);
+      } else {
+        console.log(err);
+      }
+    });
+
+    filepath = `${__dirname}/.commit_sha`;
+
+    fs.readFile(filepath, encoding, function(err,data){
+      if (!err) {
+        core.debug('read commit sha: ' + data);
+        core.setOutput("commit-sha", data);
+      } else {
+        console.log(err);
+      }
+    });
   } catch (error) {
     core.setFailed(error.message);
   }
