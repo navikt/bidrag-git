664cb13c28a59538a7557a7a0d764d8b731f07e5
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-11-28 14:24:00 +0100

    correct typo

--- release-prepare-for-mvn/src/prepare-release.sh
+++ release-prepare-for-mvn/src/prepare-release.sh
@@ -15,7 +15,7 @@ COMMIT_SHA=$(git rev-parse --short=12 HEAD)                                     
 VERSION="$SEMANTIC_VERSION-$COMMIT_SHA"                                                 # > 1.2.3-22ea0ff
 NEW_SNAPSHOT_VERSION="$MAJOR_AND_MINOR_VERSION.$NEW_PATCH_VERSION-SNAPSHOT"             # > 1.2.4-SNAPSHOT
 
-echo "semantic version: $SEMANTIC_VERSION" > .semantic-versio                           # > 1.2.3
+echo "semantic version: $SEMANTIC_VERSION" > .semantic-version                          # > 1.2.3
 echo "release version : $VERSION" > .release-version                                    # > v1.2.3-22ea0ff
 echo "commit sha      : $COMMIT_SHA" > .commit-sha                                      # > 22ea0ff
 echo "new snap version: $NEW_SNAPSHOT_VERSION" > .new-snapshot-version                  # > 1.2.4-SNAPSHOT
