fe6639af5eff2a74d235c2e976ab80d1ce23fd6d
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-11-29 10:47:05 +0100

    simplified

--- verify-mvn-dependencies/dist/src/verify.sh
+++ verify-mvn-dependencies/dist/src/verify.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-set -e
-
 mvn -B dependency:tree | tee .dependency.tree
 
-COUNT="$(cat < .dependency.tree | grep -e '|' -e '+' | grep -c SNAPSHOT)"
+DEPENNDENCIES="$(cat .dependency.tree | grep -e '|' -e +)"
+COUNT="$(echo $DEPENNDENCIES | grep -c SNAPSHOT)"
 
-if [ "$COUNT" -eq 1 ]
+echo "Found $COUNT SNAPSHOT dependencies"
+
+if [ "$COUNT" -gt 0 ]
   then
-    >&2 echo ERROR: Found a SNAPSHOT dependency
-    exit 1;
-elif [ "$COUNT" -gt 0 ]
-  then
-    >&2 echo "ERROR: Found $COUNT SNAPSHOT dependencies"
+    >&2 echo ERROR: No SNAPSHOT dependencies allowed
     exit 1;
 fi
