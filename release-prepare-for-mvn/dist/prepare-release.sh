255edbe638088527a95d03c4fb162a8b99bf148b
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-11-29 09:54:25 +0100

    semantic version -> semantic release version

--- release-prepare-for-mvn/dist/prepare-release.sh
+++ release-prepare-for-mvn/dist/prepare-release.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
 
-mvn -B help:evaluate -Dexpression=project.version | tee project_version
+mvn -B help:evaluate -Dexpression=project.version | tee .project_version
 
-SEMANTIC_VERSION_WITH_SNAPSHOT=$(cat project_version | grep -v INFO | grep -v WARNING)  # eks: 1.2.3-SNAPSHOT
+SEMANTIC_VERSION_WITH_SNAPSHOT=$(cat .project_version | grep -v INFO | grep -v WARNING) # eks: 1.2.3-SNAPSHOT
 SEMANTIC_RELEASE_VERSION=${SEMANTIC_VERSION_WITH_SNAPSHOT%-*}                           # > 1.2.3
 
 MAJOR_AND_MINOR_VERSION=${SEMANTIC_RELEASE_VERSION%.*}                                  # > 1.2
-PATCH_VERSION=$(echo "$SEMANTIC_VERSION" | sed "s/$MAJOR_AND_MINOR_VERSION.//")         # > 3
+PATCH_VERSION=$(echo "$SEMANTIC_RELEASE_VERSION" | sed "s/$MAJOR_AND_MINOR_VERSION.//") # > 3
 
 NEW_PATCH_VERSION=$(($PATCH_VERSION+1))                                                 # > 4
 COMMIT_SHA=$(git rev-parse --short=12 HEAD)                                             # eks: 22ea0ff
 
+echo "patch version '$PATCH_VERSION' and new patch version '$NEW_PATCH_VERSION'"
 RELEASE_VERSION="$SEMANTIC_RELEASE_VERSION-$COMMIT_SHA"                                 # > 1.2.3-22ea0ff
 NEW_SNAPSHOT_VERSION="$MAJOR_AND_MINOR_VERSION.$NEW_PATCH_VERSION-SNAPSHOT"             # > 1.2.4-SNAPSHOT
 
