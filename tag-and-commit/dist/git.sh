5a6da87a3a85aab4a0421455c389238c997b49b6
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-03 14:26:40 +0000

    Fresh build of actions using npm and ncc

--- tag-and-commit/dist/git.sh
+++ tag-and-commit/dist/git.sh
@@ -5,15 +5,18 @@ git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${G
 git config --global user.email "$AUTHOR_EMAIL"
 git config --global user.name "$AUTHOR_NAME"
 
-if [ -f $INPUT_TAG_FILE ]
+if [ -f .is-release-candidate ]
 then
-  TAG_CONTENT=$(cat $INPUT_TAG_FILE)
-  echo "Tagging new version with: $TAG_CONTENT"
+  if [ -f $INPUT_TAG_FILE ]
+  then
+    TAG_CONTENT=$(cat $INPUT_TAG_FILE)
+    echo "Tagging new version with: $TAG_CONTENT"
 
-  INPUT_MESSAGE=$(echo $INPUT_MESSAGE | sed "s/{}/$TAG_CONTENT/")
+    INPUT_MESSAGE=$(echo $INPUT_MESSAGE | sed "s/{}/$TAG_CONTENT/")
 
-  git tag -a "$TAG_CONTENT" -m "$INPUT_MESSAGE"
-  git push origin "$TAG_CONTENT"
+    git tag -a "$TAG_CONTENT" -m "$INPUT_MESSAGE"
+    git push origin "$TAG_CONTENT"
+  fi
 fi
 
 if ! git diff --quiet
