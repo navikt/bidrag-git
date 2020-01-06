bef5b3fb179675deea457fb39cab9230cc4bc4a0
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-06 14:53:19 +0100

    removed usused distribution

--- setup-maven/dist/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Setup maven"
-
-mkdir ~/.m2
-echo "<settings><servers><server><id>github</id><username>$GITHUB_ACTOR</username><password>$GITHUB_TOKEN</password></server></servers></settings>" > ~/.m2/settings.xml
