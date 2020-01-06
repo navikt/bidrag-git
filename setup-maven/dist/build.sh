0120afc31eb7d6d5363d8f88fb7634cb6ebc263c
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-06 12:01:32 +0000

    Fresh build of actions using npm and ncc

--- setup-maven/dist/build.sh
+++ setup-maven/dist/build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "Building maven application"
+echo "Setup maven"
 
 mkdir ~/.m2
 echo "<settings><servers><server><id>github</id><username>$GITHUB_ACTOR</username><password>$GITHUB_TOKEN</password></server></servers></settings>" > ~/.m2/settings.xml
-mvn -B install
