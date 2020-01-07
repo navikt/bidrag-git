c7f6bc0798c08970bff2c443410e97dd2d6d8df2
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-07 09:58:05 +0000

    Fresh build of actions using npm and ncc

--- setup-maven/dist/setup.sh
+++ setup-maven/dist/setup.sh
@@ -4,6 +4,36 @@ set -e
 echo "Setup maven"
 
 MAVEN_REPO=~/.m2
+MAVEN_SETTINGS="<settings>
+    <servers>
+        <server>
+           <id>fp-felles</id>
+           <username>$GITHUB_ACTOR</username>
+           <password>$GITHUB_TOKEN</password>
+      </server>
+    </servers>
+  <profiles>
+    <profile>
+      <id>default</id>
+      <repositories>
+        <repository>
+          <id>central</id>
+          <name>Maven central</name>
+          <url>https://repo.maven.apache.org/maven2</url>
+        </repository>
+        <repository>
+          <id>fp-felles</id>
+          <name>GitHub felles Apache Maven Packages</name>
+          <url>https://maven.pkg.github.com/navikt/fp-felles/</url>
+        </repository>
+      </repositories>
+    </profile>
+  </profiles>
+  <activeProfiles>
+    <activeProfile>default</activeProfile>
+  </activeProfiles>
+</settings>
+"
 
 if [ -d "$MAVEN_REPO" ]; then
   echo "Using existing $MAVEN_REPO"
@@ -12,4 +42,4 @@ else
   mkdir "$MAVEN_REPO"
 fi
 
-echo "<settings><servers><server><id>github</id><username>$GITHUB_ACTOR</username><password>$GITHUB_TOKEN</password></server></servers></settings>" > "$MAVEN_REPO"/settings.xml
+echo "$MAVEN_SETTINGS" > "$MAVEN_REPO"/settings.xml
