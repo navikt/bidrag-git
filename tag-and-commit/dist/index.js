4d988194a85f39fcc105a0d0a9c49fc6a7d5e494
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-27 07:41:26 +0000

    Fresh build of actions using npm and ncc

--- tag-and-commit/dist/index.js
+++ tag-and-commit/dist/index.js
@@ -1210,33 +1210,12 @@ const exec = __webpack_require__(496);
 async function run() {
   try {
 
-    setAuthorInformation();
-
     // Execute tag bash script
     await exec.exec(__webpack_require__.ab + "git.sh");
 
   } catch (error) {
     core.setFailed(error.message);
   }
-}
-
-function setAuthorInformation() {
-  const eventPath = process.env.GITHUB_EVENT_PATH;
-
-  if (eventPath) {
-    const { author } = require(eventPath).head_commit;
-
-    process.env.AUTHOR_NAME = author.name;
-    process.env.AUTHOR_EMAIL = author.email;
-
-  } else {
-    core.warning('No event path available, unable to fetch author info.');
-
-    process.env.AUTHOR_NAME = 'Tag & Commit Action';
-    process.env.AUTHOR_EMAIL = 'bidrag-actions.navikt@github.com';
-  }
-
-  core.info(`Using '${process.env.AUTHOR_NAME} <${process.env.AUTHOR_EMAIL}>' as author.`);
 }
 
 // noinspection JSIgnoredPromiseFromCall
