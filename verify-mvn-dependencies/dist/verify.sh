55ee1bf6cc16b836973a42b61c95c23a0d1062c8
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-02-20 18:30:08 +0000

    2020-02-20:18:30:08: Fresh build of actions with npm and ncc

--- verify-mvn-dependencies/dist/verify.sh
+++ verify-mvn-dependencies/dist/verify.sh
@@ -14,7 +14,7 @@ if [ -z "$INPUT_MAVEN_IMAGE" ]; then
   mvn -B dependency:tree | tee .dependency-tree
 else
   docker run --rm -v "$PWD":/usr/src/mymaven -v "$HOME"/.m2:/root/.m2 -w /usr/src/mymaven \
-    "$INPUT_MAVEN_IMAGE" -B dependency:tree | tee .dependency-tree
+    "$INPUT_MAVEN_IMAGE" mvn -B dependency:tree | tee .dependency-tree
 fi
 
 cat .dependency-tree | grep BUILD | grep -c SUCCESS # fails if count is 0
