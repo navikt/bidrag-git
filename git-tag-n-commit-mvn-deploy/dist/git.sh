7fbd377cda62832572b7a37566c4ab0a414d7ad0
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-03-16 14:42:54 +0000

    2020-03-16:14:42:54: Fresh build of actions with npm and ncc

--- git-tag-n-commit-mvn-deploy/dist/git.sh
+++ git-tag-n-commit-mvn-deploy/dist/git.sh
@@ -5,40 +5,26 @@ git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${G
 git config --global user.email "$AUTHOR_EMAIL"
 git config --global user.name "$AUTHOR_NAME"
 
-if [ -z "$INPUT_IS_RELEASE_FILE" ]
+if [ "$INPUT_IS_RELEASE_CANDIDATE" = "true" ]
 then
-  echo "No automatic release file is present. Tagging will not be done"
-elif [ -f "$INPUT_IS_RELEASE_FILE" ]
-then
-  if [ -f "$INPUT_TAG_FILE" ]
+  echo "Tagging new version with: $INPUT_RELEASE_VERSION"
+
+  if [ -z $INPUT_TAG_MESSAGE ]
   then
-    TAG_CONTENT=$(cat "$INPUT_TAG_FILE")
-    echo "Tagging new version with: $TAG_CONTENT"
-
-    INPUT_COMMIT_MESSAGE=$(echo "$INPUT_COMMIT_MESSAGE" | sed "s/{}/$TAG_CONTENT/")
-
-    if [ -z $INPUT_TAG_MESSAGE ]
-    then
-      >&2 echo ::error No message supplied for the tag!
-      exit 1;
-    fi
-
-    INPUT_TAG_MESSAGE=$(echo "$INPUT_TAG_MESSAGE" | sed "s/{}/$TAG_CONTENT/")
-
-    echo "Tagging release with tag message: $INPUT_TAG_MESSAGE"
-    git tag -a "$TAG_CONTENT" -m "$INPUT_TAG_MESSAGE"
-    git push origin "$TAG_CONTENT"
-  else
-      >&2 echo ::error $INPUT_TAG_FILE is not present!
-      exit 1;
+    >&2 echo ::error No message supplied for the tag!
+    exit 1;
   fi
+
+  echo "Tagging release with tag message: $INPUT_TAG_MESSAGE"
+  git tag -a "$TAG_CONTENT" -m "$INPUT_TAG_MESSAGE"
+  git push origin "$TAG_CONTENT"
 else
-  echo "No file for automatic release is present, will not release"
+  git reset --hard
 fi
 
 if ! git diff --quiet
 then
-  git diff --name-status
+  git diff status
   echo "Commiting changes with commit message: $INPUT_COMMIT_MESSAGE"
 
   git add "$INPUT_PATTERN"
