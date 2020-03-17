46d4861d3caf599cea85368e65978aab7baff2f6
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-03-17 10:00:37 +0100

    only configure, commit and tag when release candidate

--- git-tag-n-commit-mvn-deploy/git.sh
+++ git-tag-n-commit-mvn-deploy/git.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 set -e
 
-git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
-git config --global user.email "$AUTHOR_EMAIL"
-git config --global user.name "$AUTHOR_NAME"
+############################################
+#
+# Følgende skjer i dette skriptet:
+# 1)  Hvis det er en "release"-kandidat
+# 1a) Konfigurerer git, AUTHOR_EMAIL og AUTHOR_NAME blir satt av index.js (javascript delen av avtion)
+# 1b) commit endringer i INPUT_COMMIT_PATTERN
+# 1c) tag release med INPUT_TAG_MESSAGE
+#
+############################################
 
 if [ "$INPUT_IS_RELEASE_CANDIDATE" = "true" ]
 then
+  git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+  git config --global user.email "$AUTHOR_EMAIL"
+  git config --global user.name "$AUTHOR_NAME"
+
+  git diff status
+  echo "Commiting changes with commit message: $INPUT_COMMIT_MESSAGE"
+
+  git add "$INPUT_PATTERN"
+  git commit -m "$INPUT_COMMIT_MESSAGE"
+
   echo "Tagging new version with: $INPUT_RELEASE_VERSION"
 
   if [ -z $INPUT_TAG_MESSAGE ]
@@ -15,21 +31,9 @@ then
     exit 1;
   fi
 
+  git push
+
   echo "Tagging release with tag message: $INPUT_TAG_MESSAGE"
   git tag -a "$TAG_CONTENT" -m "$INPUT_TAG_MESSAGE"
   git push origin "$TAG_CONTENT"
-else
-  git reset --hard
-fi
-
-if ! git diff --quiet
-then
-  git diff status
-  echo "Commiting changes with commit message: $INPUT_COMMIT_MESSAGE"
-
-  git add "$INPUT_PATTERN"
-  git commit -m "$INPUT_COMMIT_MESSAGE"
-  git push
-else
-  echo "No files staged for commit."
 fi
