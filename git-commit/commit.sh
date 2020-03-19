6ad016a22249a17cb862923b42a7d08bb3587827
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-03-19 11:47:11 +0100

    beskrivelse av script

--- git-commit/commit.sh
+++ git-commit/commit.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -e
 
+############################################
+#
+# Følgende skjer i dette skriptet:
+# 1) Konfigurerer git for remote repository
+#    - USER_EMAIL og USER_NAME settes av javascriptet
+# 2) Når det er en endring i repository:
+#    - Sett commit message og pattern (fra javascript)
+#    - legg til endringer ihht. pattern og commit med melding
+#    - push kode til remote repository
+#
+############################################
+
 git remote set-url origin https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 git config --global user.email "$AUTHOR_EMAIL"
 git config --global user.name "$AUTHOR_NAME"
