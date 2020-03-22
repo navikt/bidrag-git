1bc58449fffd232e7ed4972c2fc3be63e63fd74e
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-03-22 18:53:16 +0100

    pattern is required parameter

--- git-commit/index.js
+++ git-commit/index.js
@@ -4,11 +4,12 @@ const exec = require("@actions/exec");
 async function run() {
   try {
     const commitMessage = core.getInput('commit_message');
+    const pattern = core.getInput('pattern');
 
     setAuthorInformation();
 
     // Execute tag bash script
-    await exec.exec(`bash ${__dirname}/commit.sh ${commitMessage}`);
+    await exec.exec(`bash ${__dirname}/commit.sh ${commitMessage} ${pattern}`);
 
   } catch (error) {
     core.setFailed(error.message);
@@ -19,7 +20,7 @@ function setAuthorInformation() {
   const eventPath = process.env.GITHUB_EVENT_PATH;
 
   if (eventPath) {
-    const { author } = require(eventPath).head_commit;
+    const {author} = require(eventPath).head_commit;
 
     process.env.AUTHOR_NAME = author.name;
     process.env.AUTHOR_EMAIL = author.email;
@@ -31,7 +32,9 @@ function setAuthorInformation() {
     process.env.AUTHOR_EMAIL = 'navikt.bidrag-actions.git-commit@github.com';
   }
 
-  core.info(`Using '${process.env.AUTHOR_NAME} <${process.env.AUTHOR_EMAIL}>' as author.`);
+  core.info(
+      `Using '${process.env.AUTHOR_NAME} <${process.env.AUTHOR_EMAIL}>' as author.`
+  );
 }
 
 // noinspection JSIgnoredPromiseFromCall
